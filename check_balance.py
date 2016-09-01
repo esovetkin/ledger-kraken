8ee97058676c0b8b3ae9b6d3b66dfca46b1246ec
Jenya Sovetkin <e.sovetkin@gmail.com>
2016-09-01 10:52:00 +0200

    added script to automatically check changes in balance

---
+++ check_balance.py
@@ -0,0 +1,37 @@
+#!/bin/env python3
+
+import krakenex
+
+import sys
+
+import json
+
+# init krakenex API
+k = krakenex.API()
+
+# load keys
+k.load_key('keys/albus-test.key')
+
+try:
+    new_bal = k.query_private('Balance')['result']
+except:
+    print("Cannot get balance")
+    sys.exit()
+    
+try:
+    with open('data/balance.json', 'r') as fp:
+        old_bal = json.load(fp)
+except:
+    print("First run, a balance file will be created")
+    old_bal = dict()
+
+if (old_bal != new_bal):
+    print("Balance has changed")
+
+    for key,value in sorted(new_bal.items()):
+        print(key + ": " + str(value))
+
+
+with open('data/balance.json','w') as fp:
+    json.dump(new_bal, fp, indent = 2)
+    
