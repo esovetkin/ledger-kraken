791bb1f738317be282e559a1978a9480df1e97de
Jenya Sovetkin <e.sovetkin@gmail.com>
2016-09-01 12:27:29 +0200

    edited check-balance script

--- check_balance.py
+++ check_balance.py
@@ -6,32 +6,36 @@ import sys
 
 import json
 
+import time
+
 # init krakenex API
 k = krakenex.API()
 
 # load keys
 k.load_key('keys/albus-test.key')
 
-try:
-    new_bal = k.query_private('Balance')['result']
-except:
-    print("Cannot get balance")
-    sys.exit()
+while (True):
+    try:
+        new_bal = k.query_private('Balance')['result']
+    except:
+        print("Cannot get balance")
+        sys.exit()
+        
+    try:
+        with open('data/balance.json', 'r') as fp:
+            old_bal = json.load(fp)
+    except:
+        print("First run, a balance file will be created")
+        old_bal = dict()
     
-try:
-    with open('data/balance.json', 'r') as fp:
-        old_bal = json.load(fp)
-except:
-    print("First run, a balance file will be created")
-    old_bal = dict()
+    if (old_bal != new_bal):
+        print("Balance has changed")
+            
+        for key,value in sorted(new_bal.items()):
+            print(key + ": " + str(value))
 
-if (old_bal != new_bal):
-    print("Balance has changed")
+    with open('data/balance.json','w') as fp:
+        json.dump(new_bal, fp, indent = 2)
 
-    for key,value in sorted(new_bal.items()):
-        print(key + ": " + str(value))
-
-
-with open('data/balance.json','w') as fp:
-    json.dump(new_bal, fp, indent = 2)
+    time.sleep(30)
     
