325ccbe1b87653372ae0cfc3f9aef686c668c558
Jenya Sovetkin <e.sovetkin@gmail.com>
2016-09-01 14:49:49 +0200

    script for quering orders

---
+++ orders.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+import krakenex
+
+import sys
+
+from tabulate import tabulate
+
+k = krakenex.API()
+
+# load keys
+k.load_key('keys/albus-test.key')
+
+
+
+orders = k.query_private('OpenOrders')
+
+orders = orders['result']['open']
+
+t = [{'pair':orders[key]['descr']['pair'],\
+      'type':orders[key]['descr']['type'],\
+      'price':orders[key]['descr']['price'],\
+      'vol':orders[key]['vol']} for key in orders.keys()]
+
+
+print(tabulate(t))
