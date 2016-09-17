3e9853e50ef105f9d8725d08026fcc91007944f1
Jenya Sovetkin <e.sovetkin@gmail.com>
2016-09-17 07:42:07 +0200

    created sql for creating database

---
+++ sql/create_db.sql
@@ -0,0 +1,42 @@
+
+-- drop all tables
+DROP TABLE pairs;
+DROP TABLE orders;
+DROP TABLE orders_log;
+
+-- creates a table with tradable pairs
+CREATE TABLE pairs (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name varchar(8) UNIQUE -- pair name, should be unique
+                    );
+
+-- fill table with tradable pairs
+INSERT INTO pairs(name) VALUES ('XXLMXXBT');
+INSERT INTO pairs(name) VALUES ('XXRPXXBT');
+INSERT INTO pairs(name) VALUES ('XLTCXXBT');
+INSERT INTO pairs(name) VALUES ('XETHXXBT');
+INSERT INTO pairs(name) VALUES ('XETCXXBT');
+INSERT INTO pairs(name) VALUES ('XDAOXXBT');
+INSERT INTO pairs(name) VALUES ('XXDGXXBT');
+INSERT INTO pairs(name) VALUES ('XXBTZEUR');
+INSERT INTO pairs(name) VALUES ('XXBTZUSD');
+
+-- creates a table with orders
+CREATE TABLE orders
+       (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       price REAL, -- price of the pair
+       time INTEGER, -- time the order was created, get from kraken
+       volume REAL, -- volume
+       pair_id INTEGER, -- pair name
+       FOREIGN KEY(pair_id) REFERENCES pairs(id) 
+       );
+
+-- creates a table with logs of 
+CREATE TABLE orders_log
+       (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       time INTEGER, -- time the info has been fetched
+       order_id INTEGER, -- id of the order
+       FOREIGN KEY(order_id) REFERENCES orders(id) 
+       );      
