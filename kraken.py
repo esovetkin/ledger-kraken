9ecec2356f76c07ad4abb3f2b9fbb14e4b138b6d
Jenya Sovetkin <e.sovetkin@gmail.com>
2016-09-19 16:50:39 +0200

    LICENSE, reorganised functions, formatting, timestamps functions

--- kraken.py
+++ kraken.py
@@ -1,5 +1,21 @@
 #!/bin/env python3
 
+# This is part of kraken-tools
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import krakenex
 
 import time
@@ -15,8 +31,16 @@ class Kraken(krakenex.API):
     """
     
     def __init__(self, key = '', secret = '', conn = None, tier = 3):
+        """Constructor for the child
+        
+        The most important part of initialising a child class is to
+        set the tier. Different tier level implies different API call
+        rate, see
+        <https://www.kraken.com/help/api#api-call-rate-limit>
+
+        """
         # call constructor of the parent
-        super(Kraken, self).__init__(self, key = key, secret = secret, conn = conn)
+        super(Kraken, self).__init__(key = key, secret = secret, conn = conn)
 
         # set counter and timestamp
         self._counter = 0
@@ -27,12 +51,14 @@ class Kraken(krakenex.API):
         
         self._tier = tier
 
+        
     def _query_cost(self, urlpath):
         """Determines cost of the urlpath query
 
         urlpath --- path specified in _query
 
         return integer
+
         """
 
         # determine cost depending on the query
@@ -49,6 +75,7 @@ class Kraken(krakenex.API):
         """Determines whether call rate limit is too high
         
         return True or False
+
         """
 
         # determine new counter: tier 2 users reduce count every 3
@@ -67,9 +94,12 @@ class Kraken(krakenex.API):
             return ceil(self._counter) >= 15
         elif 3 == self._tier or 4 == self._tier:
             return ceil(self._counter) >= 20
+
         
     def _query(self, urlpath, req = {}, conn = None, headers = {}):
         """Redefinition of low-level query handling
+
+        Arguments correspond to the parent function.
 
         """
         
@@ -77,7 +107,9 @@ class Kraken(krakenex.API):
         self._counter += self._query_cost(urlpath)
         
         while (self._if_blocked()):
+            # print debug info
             print("blocked, counter = ",self._counter)
+            # wait a second
             time.sleep(1)
             
             
@@ -96,6 +128,15 @@ class KrakenData(object):
     """
 
     def __init__(self, db_path = '', key_path = ''):
+        """Constructor
+
+        Here we initialise database connection, kraken class to
+        connect to exchange and other helping variables.
+
+        db_path --- path for the database location
+        key_path --- kraken key path
+
+        """
         # init path for db and API keys
         self._db_path = db_path
         self._key_path = key_path
@@ -112,6 +153,7 @@ class KrakenData(object):
         # get tradable pairs
         self._pairs = self._get_pairs()
 
+        
     def _get_pairs(self):
         """Get tradable pairs
 
@@ -124,7 +166,7 @@ class KrakenData(object):
 
         res = []
         try:
-            for name in c.execute("select name from pairs"):
+            for name in c.execute("SELECT name FROM pairs"):
                 res.append(name[0])
         except Exception as e:
             print("Error on getting pair names",e)
@@ -132,18 +174,25 @@ class KrakenData(object):
 
         return res
 
-    def _init_db(self):
-        """Initialising db at a given path by running a sql-script        
+    
+    def _init_db(self, path = "./createdb.sql"):
+        """Initialising db by running a given sql-script
+        
+        path --- path to the script
+        return --- nothing
         """
         c = self._dbconn.cursor()
 
+        # try to read the file
         try:
-            with open("createdb.sql",'r') as f:
+            with open(path,'r') as f:
                 script = f.read()
         except Exception as e:
             print("cannot read createdb.sql to variable",e)
             raise e
-        
+
+        # try to execute script with a database. Rollback in case of
+        # errors
         try:
             c.executescript(script)
         except Exception as e:
@@ -154,12 +203,80 @@ class KrakenData(object):
         self._dbconn.commit()
 
         
+    def _get_ServerTime(self):
+        """Get Kraken server time
+        
+        return --- float (actually, integer)
+
+        """
+
+        try:
+            t = self._kraken.query_public("Time")
+
+            if (len(t['error'])):
+                raise Exception("API error", t['error'])
+            
+            t = t['result']
+        except Exception as e:
+            print("Error during API call: Time", e)
+            raise e
+
+        return t['unixtime']
+
+    def _getTimeStamp(self, name):
+        """Query timestamp name from the "timestamps" table
+
+        name --- string of the timestamp name
+
+        return --- float. "0" in case of absense of record
+
+        """
+        
+        c = self._dbconn.cursor()
+
+        # try to query timestamp
+        try:
+            c.execute("SELECT time FROM timestamps WHERE name = ?", (str(name),))
+            return c.fetchone()[0]
+        except Exception as e:
+            print("Error during quering timestamp for '" + str(name) + "', ",e)
+            print("Assuming timestamp is zero")
+            return 0
+
+        
+    def _setTimeStamp(self, name, time):
+        """Set a timestamp in the "timestamps" table
+        
+        name --- string of the timestamp name
+        time --- new timestamp value
+
+        return --- nothing
+
+        """
+        
+        c = self._dbconn.cursor()
+
+        # try to insert in timestamp
+        try:
+            c.execute("INSERT OR REPLACE INTO timestamps(time, name) VALUES (?,?)",
+                      (time, name))
+        except Exception as e:
+            print("Error during inserting to timestamps",e)
+            self._dbconn.rollback()
+            raise e
+
+        # commit changes in database
+        self._dbconn.commit()
+
+        
     def _insert_to_OrderBook(self, new_data, time):
         """Inserts to a database a given orderbook
         
         new_data --- a new entries orderbook 
         time --- timestamp of the orderbook download time
+
         """
+        
         c = self._dbconn.cursor()
         
         # convert data to list
@@ -195,38 +312,42 @@ class KrakenData(object):
                 ''',
                  orderbook_list2)
         except Exception as e:
-            print("Error with db insertion",e)
+            print("Error with db insertion to ordersBook",e)
             self._dbconn.rollback()
             raise e
 
         # commit changes in database
-        self._dbconn.commit()
+        self._dbconn.commit()            
 
-    def _get_ServerTime(self):
-        """Get Kraken server time
+
+    def _insert_to_OrdersPrivate(self, new_data, time):
+        """Insert new orders to the database
+
+        new_data --- new data with orders
+        time --- time the orders has been fetched (Kraken time)
+
+        NOT FINISHED
         
-        return --- float (actually, integer)
         """
 
-        try:
-            t = self._kraken.query_public("Time")
+        c = self._dbconn.cursor()
 
-            if (len(t['error'])):
-                raise Exception("API error", t['error'])
-            
-            t = t['result']
-        except Exception as e:
-            print("Error during API call: Time", e)
-            raise e
 
-        return t['unixtime']
-            
+        
+        
+        # update timestamp
+        self._setTimeStamp("OrdersPrivate", time)
+        
+        # commit changes in database
+        self._dbconn.commit()            
+        
         
     def _sync_OrderBook(self, count = 500):
         """Download new order book for pairs given in self._pairs
 
         count --- number of entries in the order book to query 500
-        (apparently current maximum for kraken)
+        (apparently current maximum for kraken is 500)
+
         """
 
         new_data = {}
@@ -247,8 +368,44 @@ class KrakenData(object):
                 print("Error during API call: Depth for ", pair, e)
                 raise e
 
-            new_data[pair] = t[pair]
+           new_data[pair] = t[pair]
 
         self._insert_to_OrderBook(new_data,time)
 
-    
+        
+    def _sync_OrdersPrivate(self):
+        """Download open and closed orders and add them to the database
+
+        NOT FINISHED
+
+        """
+
+        new_data = {}
+        time = self._get_ServerTime()
+
+        # determine time period for the closed order to query
+        start = self._queryTimeStamp("OrdersPrivate")
+        end = time
+        
+        # try API call
+        try:
+            # query OpenOrders
+            t = self._kraken.query_private("OpenOrders")
+
+            if (len(t['error'])):
+                raise Exception("API error", t['error'])
+
+            t = t['result']['open']
+            
+            arg = {'start': start, 'end': end}
+
+            # query ClosedOrders (until the timestamp time)
+            
+        except Exception as e:
+            print("Error during API call: Open- Closed- Orders", e)
+            raise e
+
+        # update database
+        self._insert_to_OrdersPrivate(new_data, time)
+
+        
