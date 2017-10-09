83bdbe1a018ff8070fe0c60905c3ad3dd4c6f8be
Jenya Sovetkin <e.sovetkin@gmail.com>
2017-10-09 15:54:17 +0200

    depth, plot

--- depth.py
+++ depth.py
@@ -11,7 +11,7 @@ k = Kraken()
 arg = dict()
 
 arg['pair'] = sys.argv[1] if len(sys.argv) > 1 else 'XXBTZEUR' 
-arg['count'] = '30'
+arg['count'] = '2000'
 
 depth = k.query_public('Depth',arg)
 
@@ -19,3 +19,12 @@ try:
     print(depth_format(depth['result'],arg['pair']))
 except:
     print(depth['error'])
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+v = list(np.cumsum([float(x[1]) for x in depth['result']['XXBTZEUR']['bids']]))[::-1] + list(np.cumsum([float(x[1]) for x in depth['result']['XXBTZEUR']['asks']]))
+p = list([float(x[0]) for x in depth['result']['XXBTZEUR']['bids']])[::-1] + list([float(x[0]) for x in depth['result']['XXBTZEUR']['asks']])
+
+plt.plot(p,v)
+plt.show()
